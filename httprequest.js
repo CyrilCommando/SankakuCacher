0682c1144b5d78ece9b961406669d83d12207cfb
CyrilCommando <44797228+CyrilCommando@users.noreply.github.com>
2021-05-21 18:29:05 -0500

    1.5.3-2

---
+++ httprequest.js
@@ -0,0 +1,64 @@
+/**make XMLHttpRequest with PID and image DOM element (2nd var UNUSED)*/
+function xmlhttpReq(pid, e, isPreview = true, isContextMenu = false)
+{
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function() {
+    console.log("xhr SENT")
+  }
+
+  xhr.open("GET", "https://chan.sankakucomplex.com/post/show/"+pid);
+  xhr.responseType = "document";
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4) {
+
+      //log image url
+      console.log("image link: "+$(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("#image-link").find("img").attr("src"))
+
+      //log video url
+      console.log("video link: "+$(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("video").attr("src"))
+
+      //if image link is undefined (if not an image)
+      if ($(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("#image-link").find("img").attr("src") == undefined)
+      {
+        if (isPreview)
+        {
+          $("#preview-all_container").remove()
+          makeVideoPlayer(e)
+          $(".preview_image_or_video_tag").attr("src", $(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("video").attr("src"))
+        }
+        //preview source link
+        var v = $(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("#image-link").find("img").attr("src");
+        var y = $(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("#image-link").attr("href");
+      
+        //download link for videos (unused probably)
+        downloadLink = $(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("video").attr("src")
+      }
+      else{
+        if (isPreview)
+        {
+          $(".preview_image_or_video_tag").attr("src", $(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("#image-link").find("img").attr("src"))
+        }
+        var v = $(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("#image-link").find("img").attr("src");
+        var y = $(this.responseXML.body).find("div#content").find("div#post-view").find("div.content").find("div#post-content").find("#image-link").attr("href");
+      
+        //download link for images
+        if (y === undefined)
+        {
+          downloadLink = v
+        }
+      
+        else
+        {
+          downloadLink = y
+        }
+      }
+      
+      if (isContextMenu)
+      {
+        getTheGodDamnLink3()
+      }
+    }
+}
+}
